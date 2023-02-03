73b5c9a6e070782ce7c71b07c4845e1335a5ed57
zahranjamali <122424335+zahranjamali@users.noreply.github.com>
2023-02-03 13:00:54 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar () {
 
 def buildImage(){
     echo "building the application"
-                    withCredentials([usernamePassword(credentialsID: 'zahran', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+                    withCredentials([usernamePassword(credentialsID: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
                         sh 'docker build -t zahranjamali/my-repo:jma-1.0 .'
                         sh "echo $PASS | docker login -u $USER --password-stdin"
                         sh 'docker push zahranjamali/my-repo:jma-1.0'
